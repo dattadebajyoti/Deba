0fa94689fcf1dc54e95d93fa8452fbd563bbf2ec
dattadebajyoti <debajyoti95datta@gmail.com>
2017-07-31 17:47:23 +0530

    second

---
+++ checkoddeven.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+ int b;
+ printf("enter any integer:");
+ scanf("%d",&a);
+ if(a%2==0)
+ printf("even");
+ else
+ printf("odd");
+ getch();
+ return 0;
+}
+
+
+
