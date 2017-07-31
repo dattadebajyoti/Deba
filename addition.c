771212b3a409a1801944358e7671725507c27eca
dattadebajyoti <debajyoti95datta@gmail.com>
2017-07-31 18:33:54 +0530

    file added

---
+++ addition.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+ int a,b,c;
+ printf("enter 2 integers:\n");
+ scanf("%d%d",&a,&b);
+ c= a+b;
+ printf("sum is %d",c);
+ getch();
+ return 0;
+}
+ 
