7e199519b1bf30c1a1b6fee37b215b5b2c1523a9
dattadebajyoti <debajyoti95datta@gmail.com>
2017-07-31 18:19:28 +0530

    changes done in checkoddeven

--- checkoddeven.c
+++ checkoddeven.c
@@ -3,8 +3,10 @@
 int main()
 {
  int b;
+ printf("this is an artificial world made to serve for humans..................../n");
  printf("enter any integer:");
  scanf("%d",&a);
+ printf("doing it");
  if(a%2==0)
  printf("even integer");
  else
