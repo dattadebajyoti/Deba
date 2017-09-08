9ce4a983a0436b864cba6cbdcb300e2839a8dcd3
dattadebajyoti <debajyoti95datta@gmail.com>
2017-09-08 16:33:13 +0530

    message

---
+++ chatapp/chatNew/sample/form.js
@@ -0,0 +1,77 @@
+//var socket=io();
+//function to insert into database
+$('#insert').click(submithandler);
+
+function submithandler() {
+  $.ajax({
+    url: '/inserting',
+    type: 'POST',
+    data: {
+      name: form1.name.value,
+      pwd: form1.pwd.value
+    },
+    sucess: function(data) {
+      alert("sucess");
+    }
+  });
+}
+
+
+
+//function to login
+function login() {
+  var username = prompt("enter username");
+  var password = prompt("enter password");
+  $.ajax({
+    url: '/signingin',
+    type: 'POST',
+    data: {
+      name: username,
+      pwd: password
+    }
+  }).done(function(result) {
+    console.log(result);
+    if (result.data == "false") {
+      //socket.emit('user name',username);
+      alert("Login Successfully");
+      localStorage.setItem("username", username);
+      window.location.href = "chatApp.html";
+    } else {
+      alert("Sorry!! please create your account first");
+    }
+  })
+}
+
+
+function session() {
+  return new Promise(function(resolve, reject) {
+    var promise = $.ajax({
+      url: '/checkUserLogin',
+      type: 'GET',
+      headers: {
+        "Content-Type": "application/json"
+      }
+    }).done(function(data) {
+      debugger;
+      var name = data.name;
+      localStorage.setItem("username", name);
+      if (data.isLogin) {
+
+        //document.getElementById("status").innerHTML = name + "<br>Online";
+        database();
+      } else {
+        document.write("Already Login");
+        logout();
+        // if (window.location.href !== "index.html") {
+        //   window.location.href = "index.html";
+        // }
+      }
+      resolve(data);
+    });
+
+  });
+}
+
+function logout() {
+  window.location.href = "index.html";
+}
---
+++ chatapp/chatNew/server.js
@@ -0,0 +1,151 @@
+var express =require("express");
+var bodyParser=require("body-parser");
+var path = require("path");
+var app=express();
+var fs=require('fs');
+var http=require('http').Server(app);
+var io=require('socket.io')(http);
+var PORT = process.env.PORT || 3004;
+//app.use(bodyParser.json());
+
+var session = require('express-session');
+app.use(session({
+  secret: 'ssshhhhh',
+  saveUninitialized: true,
+  resave: true
+}));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use("/",express.static('./sample/'));
+
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://localhost:27017/mydb";
+
+MongoClient.connect(url,function(err,db)
+{
+  if (err) throw err;
+  var json;
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({extended: true}));
+
+
+  app.post('/inserting',function(req, res)
+  {
+    var result = {status:true,message:"Successfully added"};
+    res.setHeader('Content-Type', 'application/json');
+    //console.log(req.body.name+" "+req.body.pwd);
+    //  MongoClient.connect(url, function(err,db) {
+    //   if (err) throw err;
+      var myObj=[
+      { name : req.body.name, pwd : req.body.pwd}
+      ];
+      db.collection("userDetails").insertOne(myObj, function(err, res) {
+        if (err) throw err;
+        console.log("Account Created");
+        //console.log("Account details: "+res.result);
+        // db.close();
+      })
+      db.collection("userDetails").find({}, { _id: false }).toArray(function(err, result) {
+        if (err) throw err;
+        console.log(result);
+        // db.close();
+      });
+  //  });
+  });
+
+
+  app.post('/signingin', function(req, res)
+  {
+    res.setHeader("Content-Type","application/json");
+    var result = {status: true,message: "Successfully added"};
+  // MongoClient.connect(url,function(err,db) {
+  //   if (err) throw err;
+      db.collection("userDetails").find({name:req.body.name,pwd:req.body.pwd}).toArray(function(err,data)
+      {
+        if(!err)
+        if(data.length!=0)
+        {
+          req.session.name = req.body.name;
+          user=req.body.name;
+          console.log("hiii");
+          res.json({data:"false"});
+        }
+        else
+        {
+          res.json({data:"true"});
+        }
+      });
+  // });
+  });
+  //db.close();
+});
+
+
+
+
+io.on('connection',function(socket)
+{
+  socket.on('chat message', function(obj){
+    var msg = obj.msg;
+    var username = obj.username;
+    var time = obj.time;
+    console.log(msg);
+    //console.log(user);
+    MongoClient.connect(url, function(err,db) {
+     myObj=[{ message : msg, userName: username, timeOfMessage:time}];
+     db.collection("storeMessage").insertMany(myObj, function(err, res) {
+       if (err) throw err;
+       console.log(user);
+       console.log("Message Stored");
+      //  db.close();
+     })
+     db.collection("storeMessage").find({},{ _id: false }).toArray(function(err, result) {
+       if (err) throw err;
+       console.log(result);
+      //  db.close();
+     });
+  });
+    // if(err) throw err;
+    io.sockets.emit('chat message', obj);
+    console.log("/////////////////////////////this is "+obj.time);
+    //io.emit('username',user);
+    //console.log("message: " + msg);
+  });
+});
+//
+
+app.get('/checkUserLogin', function(req, res)
+ {
+   var session=req.session;
+   if(session.name)
+   {
+     res.json({name:session.name,isLogin:true});
+   }
+   else
+   {
+     res.json({name:session.name,isLogin:false});
+   }
+});
+
+
+
+
+app.get('/get', function(req, res)
+{
+  console.log(url);
+  MongoClient.connect(url, function(err,db) {
+    // console.log(db);
+    db.collection("storeMessage").find({}, {_id:false, message:true, userName:true, timeOfMessage:true}).toArray(function(err,data){
+      if(err)throw err;
+      // console.log(data);
+      res.send(data);
+    });
+    // db.close();
+   })
+});
+
+
+
+http.listen(PORT,function()
+{
+  console.log("server is listening to %s port",PORT);
+});
