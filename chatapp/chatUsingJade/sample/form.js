96303ae26591f5657d0d917a56ce5c236902618d
dattadebajyoti <debajyoti95datta@gmail.com>
2017-09-19 17:19:05 +0530

    done

---
+++ chatapp/chatUsingJade/sample/form.js
@@ -0,0 +1,96 @@
+//var socket=io();
+//function to insert into database
+$('#insert').click(submithandler);
+
+function submithandler() {
+  $.ajax({
+    url: '/inserting',
+    type: 'POST',
+    data: {
+      name: form1.name.value,
+      pwd: form1.pwd.value
+    },
+    sucess: function(data) {
+      alert("sucess");
+    }
+  });
+}
+
+
+
+//function to login
+function login() {
+  var username=form2.username.value;
+  var password=form2.password.value;
+  $.ajax({
+    url: '/signingin',
+    type: 'POST',
+    data: {
+      name: username,
+      pwd: password
+    }
+  }).done(function(result) {
+    console.log(result);
+    if (result.data == "false") {
+      //socket.emit('user name',username);
+      alert("Login Successfully");
+      localStorage.setItem("username", username);
+      window.location.href = "chatApp.html";
+    } else {
+      alert("Sorry!! please create your account first");
+    }
+  })
+}
+
+
+function session() {
+  return new Promise(function(resolve, reject) {
+    var promise = $.ajax({
+      url: '/checkUserLogin',
+      type: 'GET',
+      headers: {
+        "Content-Type": "application/json"
+      }
+    }).done(function(data) {
+      //debugger;
+      var name = data.name;
+      console.log(name);
+      localStorage.setItem("username", name);
+      if (data.isLogin) {
+        //alert("logged in");
+        document.getElementById("status").innerHTML = name + "<br>Online";
+        //window.location.href = "chatApp.html";
+        //database();
+      }
+      else {
+      //  console.log("hellow");
+        //logout();
+        if(window.location.href !== "index.html") {
+          window.location.href = "index.html";
+        }
+      }
+      resolve(data);
+    });
+
+  });
+}
+
+function logout() {
+  console.log("ok doing log out");
+  $.ajax({
+    url : '/endSession',
+    type : 'GET',
+    headers : {
+      "Content-Type" : "Application/Json"
+    }
+  }).done(function(result)
+        {
+          console.log(result.data);
+          if(result.data == "false")
+          {
+            alert("Logging out");
+            console.log(result.data);
+            window.location.href = "index.html";
+          }
+        });
+}
